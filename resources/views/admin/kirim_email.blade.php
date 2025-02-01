919dfcc09a8082e2de5fcbcbd0a5fe3359be5dd4
OrizaAria <orizadioaria1705@gmail.com>
2025-02-01 19:24:50 +0700

    database

--- resources/views/admin/kirim_email.blade.php
+++ resources/views/admin/kirim_email.blade.php
@@ -1,9 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 
 <head>
     <base href="/public">
     @include('admin.css')
+    <style>
+        /* Styling untuk memastikan label dan input lebih dekat */
+        .form-group {
+            margin-bottom: 15px;
+            /* Mengurangi margin bawah antara setiap form group */
+        }
+
+        .form-control-label {
+            font-weight: bold;
+            margin-bottom: 5px;
+            /* Mengurangi jarak antara label dan input */
+            color: #333;
+            text-align: left;
+        }
+
+        .form-control {
+            padding: 8px 12px;
+            /* Mengurangi padding untuk input agar lebih kompak */
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+
+        /* Agar textarea juga lebih kompak */
+        textarea.form-control {
+            padding: 8px 12px;
+            resize: vertical;
+        }
+
+        /* Menambahkan sedikit ruang antara tombol dan form */
+        .btn-outline-primary {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -16,54 +49,58 @@
             <div class="page-header">
                 <div class="container-fluid">
                     <center>
-                        <h1 style="font-size: 30px; font-weight:bold;">Email Dikirim Ke {{ $data->name }}</h1>
-                        <form action="{{ url('mail/' . $data->id) }}" method="POST">
-                            @csrf
-                            <div class="form-group row">
-                                <label class="col-sm-3 form-control-label"> Greeting</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="greeting" class="form-control">
-                                </div>
+                        <h1 style="font-size: 30px; font-weight:bold; padding-bottom: 30px;">Email Dikirim Ke
+                            {{ $data->name }}</h1>
+                    </center>
+                    <form action="{{ url('mail/' . $data->id) }}" method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Greeting</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="greeting" class="form-control">
                             </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 form-control-label">Isi Email</label>
-                                <div class="col-sm-9">
-                                    <textarea class="form-control" name="isi" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Isi Email</label>
+                            <div class="col-sm-9">
+                                <textarea class="form-control" name="isi" id="exampleFormControlTextarea1" rows="3"></textarea>
                             </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 form-control-label">Action Text</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="action_text" class="form-control">
-                                </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Action Text</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="action_text" class="form-control">
                             </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 form-control-label">Action Url</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="action_url" class="form-control">
-                                </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Action Url</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="action_url" class="form-control">
                             </div>
-                            <div class="form-group row">
-                                <label class="col-sm-3 form-control-label">Penutupan</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="penutupan" class="form-control">
-                                </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Penutupan</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="penutupan" class="form-control">
                             </div>
+                        </div>
+                        <center>
                             <div class="line"></div>
                             <div class="form-group row">
-                                <div class="col-sm-9 ml-auto">
+                                <div style="padding-top: 20px;" class="col-sm-9 ml-auto">
                                     <button type="submit" value="" class="btn btn-outline-primary">Kirim
                                         Email</button>
                                 </div>
                             </div>
-                        </form>
-                    </center>
+                        </center>
+                    </form>
                 </div>
             </div>
         </div>
         @include('admin.footer')
         <!-- JavaScript files-->
         @include('admin.js')
+    </div>
 </body>
 
 </html>
--- si_hotel.sql
+++ si_hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Jan 2025 pada 13.13
+-- Waktu pembuatan: 01 Feb 2025 pada 13.20
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,6 +24,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_kamar` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'menunggu',
+  `tanggal_masuk` varchar(255) DEFAULT NULL,
+  `tanggal_keluar` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `id_kamar`, `nama`, `email`, `phone`, `status`, `tanggal_masuk`, `tanggal_keluar`, `created_at`, `updated_at`) VALUES
+(1, '1', 'user', 'user@gmail.com', '0892374843', 'Diterima', '2025-01-26', '2025-01-27', '2025-01-26 02:29:28', '2025-01-26 02:30:37'),
+(2, '2', 'user', 'user@gmail.com', '0892374843', 'menunggu', '2025-01-28', '2025-01-29', '2025-01-26 02:29:52', '2025-01-26 02:29:52'),
+(3, '3', 'user', 'user@gmail.com', '0892374843', 'Ditolak', '2025-01-30', '2025-01-31', '2025-01-26 02:30:12', '2025-01-26 02:30:41');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `massage` longtext NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `phone`, `massage`, `created_at`, `updated_at`) VALUES
+(1, 'user', 'user@gmail.com', '0897267346', 'hello', '2025-01-26 00:57:59', '2025-01-26 00:57:59'),
+(2, 'admin', 'admin@gmail.com', '0892374843', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod ipsum sit amet aliquet interdum. Curabitur ultrices nisl enim, sit amet tempus elit maximus placerat. Pellentesque eu mi quis nunc bibendum tincidunt ac vel turpis. Donec semper efficitur interdum. Praesent vel nibh varius, vehicula elit ac, semper ex. Mauris nec urna lacus.', '2025-01-26 01:04:48', '2025-01-26 01:04:48'),
+(3, 'oriza dio aria', 'aink.strez44@yahoo.com', '0892367456', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod ipsum sit amet aliquet interdum. Curabitur ultrices nisl enim, sit amet tempus elit maximus placerat. Pellentesque eu mi quis nunc bibendum tincidunt ac vel turpis. Donec semper efficitur interdum. Praesent vel nibh varius, vehicula elit ac, semper ex. Mauris nec urna lacus.', '2025-01-26 01:07:17', '2025-01-26 01:07:17'),
+(4, 'user', 'werg@gm.k', '0897267346', '<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">', '2025-01-26 01:35:41', '2025-01-26 01:35:41'),
+(5, 'user', 'user@gmail.com', '0897267346', 'iam venom', '2025-01-26 01:41:27', '2025-01-26 01:41:27'),
+(6, 'user', 'user@gmail.com', '0892374843', 'Massage ajg', '2025-01-26 03:14:53', '2025-01-26 03:14:53');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `failed_jobs`
 --
 
@@ -36,6 +92,31 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `galleries`
+--
+
+CREATE TABLE `galleries` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `gambar` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `galleries`
+--
+
+INSERT INTO `galleries` (`id`, `gambar`, `created_at`, `updated_at`) VALUES
+(1, '1737883903.png', '2025-01-26 02:31:43', '2025-01-26 02:31:43'),
+(2, '1737883910.jpg', '2025-01-26 02:31:50', '2025-01-26 02:31:50'),
+(3, '1737883918.jpg', '2025-01-26 02:31:58', '2025-01-26 02:31:58'),
+(4, '1737883929.jpg', '2025-01-26 02:32:09', '2025-01-26 02:32:09'),
+(5, '1737883940.jpg', '2025-01-26 02:32:20', '2025-01-26 02:32:20'),
+(6, '1737883948.jpg', '2025-01-26 02:32:28', '2025-01-26 02:32:28');
 
 -- --------------------------------------------------------
 
@@ -60,7 +141,29 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2019_08_19_000000_create_failed_jobs_table', 1),
 (5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (6, '2025_01_13_102614_create_sessions_table', 1),
-(7, '2025_01_18_064339_create_rooms_table', 1);
+(7, '2025_01_18_064339_create_rooms_table', 1),
+(8, '2025_01_23_123918_create_bookings_table', 1),
+(9, '2025_01_25_075150_add_status_field_to_bookings', 1),
+(10, '2025_01_26_054932_create_galleries_table', 1),
+(11, '2025_01_26_074048_create_contacts_table', 1),
+(12, '2025_01_31_081951_create_notifications_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` char(36) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `notifiable_type` varchar(255) NOT NULL,
+  `notifiable_id` bigint(20) UNSIGNED NOT NULL,
+  `data` text NOT NULL,
+  `read_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -111,6 +214,15 @@ CREATE TABLE `rooms` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `nama_kamar`, `gambar`, `deskripsi`, `harga`, `type_kamar`, `wifi`, `created_at`, `updated_at`) VALUES
+(1, 'Kamar Rendah', '1737881060.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod ipsum sit amet aliquet interdum. Curabitur ultrices nisl enim, sit amet tempus elit maximus placerat. Pellentesque eu mi quis nunc bibendum tincidunt ac vel turpis. Donec semper efficitur interdum. Praesent vel nibh varius, vehicula elit ac, semper ex. Mauris nec urna lacus.', '200000', 'rendah', 'yes', '2025-01-26 01:44:20', '2025-01-26 01:44:20'),
+(2, 'Kamar Sedang', '1737881078.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod ipsum sit amet aliquet interdum. Curabitur ultrices nisl enim, sit amet tempus elit maximus placerat. Pellentesque eu mi quis nunc bibendum tincidunt ac vel turpis. Donec semper efficitur interdum. Praesent vel nibh varius, vehicula elit ac, semper ex. Mauris nec urna lacus.', '500000', 'sedang', 'yes', '2025-01-26 01:44:38', '2025-01-26 01:44:38'),
+(3, 'Kamar Tinggi', '1737881091.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod ipsum sit amet aliquet interdum. Curabitur ultrices nisl enim, sit amet tempus elit maximus placerat. Pellentesque eu mi quis nunc bibendum tincidunt ac vel turpis. Donec semper efficitur interdum. Praesent vel nibh varius, vehicula elit ac, semper ex. Mauris nec urna lacus.', '1000000', 'tinggi', 'yes', '2025-01-26 01:44:51', '2025-01-26 01:44:51');
+
 -- --------------------------------------------------------
 
 --
@@ -131,7 +243,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('OHRphZoYCsDsSpfy1pB8fiELPffejCTJxmrr0ApG', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoieWhKQVJKdW56QmUzR0daM0hna0JEQ2FwaTdSWEtUMUIzaEZzcVJiUiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9kYXRhX2thbWFyIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1737288485);
+('wpVqYOR3CfOirIW3wWJBtiknOXFTCaEzTKIryeef', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiVmFHYjNmWHo2Z3dVWll4VzRwUEF3Z1BjR3NBckQ2SXV6cmpWa0xySCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzU6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9raXJpbV9lbWFpbC8yIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1738324325);
 
 -- --------------------------------------------------------
 
@@ -162,11 +274,24 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `phone`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `usertype`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@gmail.com', NULL, '+6210398874', '$2y$12$bH3jrtzlTJ0hABnmULFVgukFFrV3wAfcp5LBZ7/VolnXCg/23U01C', NULL, NULL, NULL, 'admin', NULL, NULL, NULL, '2025-01-19 03:12:16', '2025-01-19 03:12:16');
+(1, 'admin', 'admin@gmail.com', NULL, '0892374843', '$2y$12$c8PhHokGpEI7YN9/jKzd9uSh/HQ56eAeOzkrRvYK6gNpxL115Uv0i', NULL, NULL, NULL, 'admin', NULL, NULL, NULL, '2025-01-26 01:42:49', '2025-01-26 01:42:49'),
+(2, 'user', 'user@gmail.com', NULL, '0892374843', '$2y$12$aGAqFSxZ52ub80AV3cg4V.aSyUT/cRDhgoKNaEelLvzYBv5KM7qYi', NULL, NULL, NULL, 'user', NULL, NULL, NULL, '2025-01-26 01:43:24', '2025-01-26 01:43:24');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -176,10 +301,23 @@ ALTER TABLE `failed_jobs`
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
+-- Indeks untuk tabel `galleries`
+--
+ALTER TABLE `galleries`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notifications_notifiable_type_notifiable_id_index` (`notifiable_type`,`notifiable_id`);
 
 --
 -- Indeks untuk tabel `password_reset_tokens`
@@ -221,16 +359,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `galleries`
+--
+ALTER TABLE `galleries`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `personal_access_tokens`
@@ -242,13 +398,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT untuk tabel `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
